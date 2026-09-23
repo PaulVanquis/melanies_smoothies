061105f62283794b204dae603c9c0c3ba01f9d82
PaulVanquis <paul.richardson@vanquis.com>
2026-09-23 13:02:35 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,6 +45,10 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered '+name_on_order+'!', icon="✅")
 
-import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+# Call the SmoothieFroot API
+smoothiefroot_response = requests.get(
+"https://my.smoothiefroot.com/api/fruit/watermelon"
+)
+ 
+# Display API response
+st.text(smoothiefroot_response.text)
