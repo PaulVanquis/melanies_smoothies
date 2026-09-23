fae3d2c271357b41cc4512ce24b7283debbb475d
PaulVanquis <paul.richardson@vanquis.com>
2026-09-23 14:02:52 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,16 +45,7 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered '+name_on_order+'!', icon="✅")
 
-import requests
-
-smoothiefroot_response = requests.get(
-    "https://my.smoothiefroot.com/api/fruit/watermelon"
-)
-
-# Optional: display the raw JSON response
+import requests  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 # st.text(smoothiefroot_response.json())
-
-sf_df = st.dataframe(
-    data=smoothiefroot_response.json(),
-    use_container_width=True
-)
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
